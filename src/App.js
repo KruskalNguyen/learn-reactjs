45a747d40dadf39abf7401df0d02eac064b353ae
Dev Metin <khangnghuyen72@gmail.com>
2023-08-13 21:36:27 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,114 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function LeftContain() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="LeftContain col-lg-8">
+              <div className="fxt-column-wrap justify-content-between">
+                <div className="fxt-animated-img">
+                  <div className="fxt-transformX-L-50 fxt-transition-delay-10">
+                    <img src="img/figure/bg34-1.png" alt="Animated Image" />
+                  </div>
+                </div>
+                <div className="fxt-transformX-L-50 fxt-transition-delay-3">
+                  <a href="login-34.html" className="fxt-logo">
+                  <h1
+                    style={{
+                      color: "rgb(85, 85, 255)",
+                      font: "bold",
+                      fontWeight: 600,
+                      fontSize: 50
+                    }}
+                  >
+                    Kruskal Nguyễn
+                  </h1>
+                  </a>
+                </div>
+                <div className="fxt-transformX-L-50 fxt-transition-delay-5">
+                  <div className="fxt-middle-content">
+                    <h1 className="fxt-main-title">Đăng nhập</h1>
+                    <div className="fxt-switcher-description1">
+                      Nếu bạn chưa có tài khoản Bạn có thể
+                      <a href="register-34.html" className="fxt-switcher-text ms-2">
+                        Đăng ký
+                      </a>
+                    </div>
+                  </div>
+                </div>
+                <div className="fxt-transformX-L-50 fxt-transition-delay-7">
+                  <div className="fxt-qr-code">
+                    <img src="img/elements/qr-login-34.png" alt="QR" />
+                  </div>
+                </div>
+              </div>
+      </div>
   );
 }
 
-export default App;
+export function LoginContain(){
+    return (
+      <div className="LoginContain col-lg-4">
+            <div className="fxt-column-wrap justify-content-center">
+              <div className="fxt-form">
+                <form method="POST">
+                  <div className="form-group">
+                    <input
+                      type="email"
+                      id="email"
+                      className="form-control"
+                      name="email"
+                      placeholder="Enter Email or Mobile Number"
+                      required="required"
+                    />
+                  </div>
+                  <div className="form-group">
+                    <input
+                      id="password"
+                      type="password"
+                      className="form-control"
+                      name="password"
+                      placeholder="********"
+                      required="required"
+                    />
+                    <i
+                      toggle="#password"
+                      className="fa fa-fw fa-eye toggle-password field-icon"
+                    />
+                  </div>
+                  <div className="form-group">
+                    <div className="fxt-switcher-description2 text-right">
+                      <a href="forgot-password-34.html" className="fxt-switcher-text">
+                        Recovery Password
+                      </a>
+                    </div>
+                  </div>
+                  <div className="form-group">
+                    <button type="submit" className="fxt-btn-fill">
+                      Sign In
+                    </button>
+                  </div>
+                </form>
+              </div>
+              <div className="fxt-style-line">
+                <span>Or Continus with</span>
+              </div>
+              <ul className="fxt-socials">
+                <li className="fxt-google">
+                  <a href="#" title="google">
+                    <i className="fab fa-google-plus-g" />
+                  </a>
+                </li>
+                <li className="fxt-apple">
+                  <a href="#" title="apple">
+                    <i className="fab fa-apple" />
+                  </a>
+                </li>
+                <li className="fxt-facebook">
+                  <a href="#" title="Facebook">
+                    <i className="fab fa-facebook-f" />
+                  </a>
+                </li>
+              </ul>
+            </div>
+      </div>
+    );
+}
+
+// export default LeftContain;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import LeftContain from './App';  
 import reportWebVitals from './reportWebVitals';
+import { LoginContain } from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <LeftContain />
+    <LoginContain />
   </React.StrictMode>
 );
 
